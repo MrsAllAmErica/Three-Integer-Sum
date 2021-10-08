ce868e26da53bcaa5f227dc2c94cf4a5fefd6888
Erica A <91516989+MrsAllAmErica@users.noreply.github.com>
2021-10-08 16:29:57 -0700

    Update Three_Integer_Sum.cpp

--- Three_Integer_Sum.cpp
+++ Three_Integer_Sum.cpp
@@ -1,1 +1,20 @@
 #include <cstdio>
+
+int main ()
+{
+    int n1, n2, n3, sum
+      
+      printf ("Please enter any number: ");
+      scanf ("%d, &n1);
+             
+      printf ("\nPlease enter another number: ");
+      scanf ("%d", &n2);
+             
+      printf ("\nPlease enter one last number: ");
+      scanf ("%d", &n3);
+      
+      sum = n1 + n2 + n3;
+      printf ("Thank you! The sum is %d, have a nice day!", sum);
+             return0;
+             
+             }
